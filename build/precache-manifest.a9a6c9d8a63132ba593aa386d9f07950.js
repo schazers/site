be8663dc0fb2442f04b9ecd1bd6bddb1c6b1372d
schazers <schazers@gmail.com>
2019-09-25 15:05:38 -0700

    make a build

---
+++ build/precache-manifest.a9a6c9d8a63132ba593aa386d9f07950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b126a04f524358e077fd250eb74f26ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e633d9f3dbac2d6c25db",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a12fe28fc672a76c4ede",
+    "url": "/static/js/2.5fe7d40c.chunk.js"
+  },
+  {
+    "revision": "e633d9f3dbac2d6c25db",
+    "url": "/static/js/main.336653da.chunk.js"
+  },
+  {
+    "revision": "93a457f2e2bc72221bfe",
+    "url": "/static/js/runtime-main.aa8ed522.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
